c97978f24d9a62b4741b6c1a87b7ca2e882d2e12
Helder Santos <hps150193@gmail.com>
2025-08-17 05:41:19 -0300

    add MyBlogsViewTests

--- blog/tests.py
+++ blog/tests.py
@@ -1,32 +1,79 @@
 from django.test import TestCase
-from .models import Blog
 from django.urls import reverse
+from urllib.parse import urlencode
+from django.contrib.auth import get_user_model, get_user
+from .models import Blog
 
 
-# class BlogModelTests(TestCase):
-#     def x(self):
-#         self.assertIs(1, 1)
+User = get_user_model()
 
 
 class MyBlogsViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create 3 users.
+        cls.user = User.objects.create_user(
+            username='Testuser',
+            email='testemail@example.com',
+            password='mypassword123')
+        cls.user2 = User.objects.create_user(
+            username='Testuser2',
+            email='testemail2@example.com',
+            password='mypassword123')
+        cls.user3 = User.objects.create_user(
+            username='Testuser3',
+            email='testemail3@example.com',
+            password='mypassword123')
+        
+        # Create 3 blogs for user and user2.
+        # user3 has no blogs.
+        for user in cls.user, cls.user2:
+            for _ in range(3):
+                Blog.objects.create(user=user, title='Test')
+
     def test_unauthenticated_user_is_redirected_to_login_url(self):
-        """
-        If an unauthenticated user tries to access the ``my_blog`` 
-        url, they should be redirected to the login page.
-        """
-        response = self.client.get(reverse("blog:my_blogs"))
-        self.assertEqual(response.status_code, 302)
-        self.assertIn(reverse("blog:login"), response.url)
+        # Ensures that no user is logged in.
+        self.client.logout()
+        self.assertIsNone(self.client.session.get("_auth_user_id"))
 
-    # authenticated users see their blogs?
-    def test_authenticated_user_receive_their_blogs(self):
-        pass
+        my_blogs_url = reverse("blog:my_blogs")
+        response = self.client.get(my_blogs_url)
+        query_string = urlencode({'next': my_blogs_url})
+        login_url = reverse("accounts:login")
+        self.assertRedirects(response, f"{login_url}?{query_string}")
 
-    # authenticated users see others blogs?
-    # authenticated users see "no blogs" if there's no blogs?
+    def test_authenticated_user_receive_correct_blogs(self):
+        for user, n_blogs in ((self.user, 3), (self.user2, 3), (self.user3, 0)):
+            # Ensures that a new user has logged in.
+            self.client.logout()
+            self.client.force_login(user)
+            self.assertEqual(user, get_user(self.client))
+
+            # Ensures that the user has the expected number of blogs.
+            blogs = Blog.objects.filter(user=user)
+            self.assertEqual(len(blogs), n_blogs)
+
+            response = self.client.get(reverse("blog:my_blogs"))
+
+            # Ensures that the QuerySet object provided by the view’s 
+            # context has the expected number of blogs.
+            self.assertEqual(len(response.context['blogs']), n_blogs)
+
+            # Ensures that the number of user blogs matches the number
+            # of blogs provided in the view’s context, and that they 
+            # have the same IDs.        
+            self.assertCountEqual(
+                blogs.values_list("id", flat=True),
+                response.context['blogs'].values_list("id", flat=True))
+        
+        # Ensures that a total of 6 blogs were created.
+        self.assertEqual(len(Blog.objects.all()), 6)
+
+        # Ensures tha the user with no blogs see the correct message.
+        self.assertContains(response, 'No blogs added...')
 
 
-
+#commit
 # new_blog
     # unauthenticated user receives 404? redirected to login?
     # authenticated user can create blog for other user? permissions?
@@ -65,9 +112,12 @@ class MyBlogsViewTests(TestCase):
     # after registering, the system automatically creates a Blog instance for the user?
     # after registering, the system automatically login the user?
     # succesfully registered?
+    # create first blog automatically?
 
 
 # login
+    # login redireciona onde?
+    # se redirecionado para login, pra onde sou redirecionado depois? para o redirecionamento normal de login, ou para a url que tentei acessar antes de estar logado?
     # after login, redirected to my_blogs (or other page)?
     # succesfully logged?
 
